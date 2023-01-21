38def502bf2a30c5370814a6aec082bf129466d8
lucho9595 <lucianocoronel48@gmail.com>
2023-01-21 20:17:58 -0300

    add SetAvatar

--- client/chat-app/src/pages/SetAvatar.jsx
+++ client/chat-app/src/pages/SetAvatar.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-
-
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function SetAvatar() {
+  const navigate = useNavigate();
+  const aleatorio = Math.random()
 
+  const api = `https://api.multiavatar.com/${aleatorio}`
 
   return (
     <div>
-    
+      <Container>
+        <span>{api}</span>
+      </Container>
+      <ToastContainer />
     </div>
   )
 }
+
+const Container = styled.div``;
