44dc5e3236bf2a04bb97fa5ecda5b8a55a207bcd
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2023-01-24 16:38:39 -0300

    empezar el chat

--- client/chat-app/src/components/Contacts.jsx
+++ client/chat-app/src/components/Contacts.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import styled from "styled-components";
+import Logo from "../assets/logo.png";
 
-function Contacts({contacts, currentUser}) {
+function Contacts({ contacts, currentUser }) {
   return (
-    <div>
-      Contacts
-    </div>
-  )
-};
+    <Container>
+      <>
+        <div className="title">
+          <img src={Logo} alt="logo chat" />
+          <h1>chatapp</h1>
+        </div>
+      </>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  .title {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+  }
+  h1 {
+    color: white;
+    text-transform: uppercase;
+    font-size: 15px;
+  }
+  img {
+    width: 35px;
+  }
+`;
 
 export default Contacts;
