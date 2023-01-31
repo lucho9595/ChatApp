2ffff8768a1807e1ccc2f584664ef3e21274be22
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2023-01-31 16:57:47 -0300

    maquetado de settings

--- client/chat-app/src/pages/EditUser.jsx
+++ client/chat-app/src/pages/EditUser.jsx
@@ -14,6 +14,10 @@ function EditUser() {
     navigate("/login");
   };
 
+  const handleReturn = () => {
+    navigate("/");
+  };
+
   useEffect(() => {
     if (!localStorage.getItem("chat-app-user")) {
       navigate("/login");
@@ -24,37 +28,51 @@ function EditUser() {
 
   return (
     <>
+      <Title>
+        <div className="title">
+          <h1>Profile Settings</h1>
+        </div>
+      </Title>
       <Container>
         <div className="container">
-          <div class="card h-100">
+          <div class="card1">
             <div className="user-profile">
               <div className="user-avatar">
-                <img
-                  src="https://bootdey.com/img/Content/avatar/avatar1.png"
-                  alt="Maxwell Admin"
-                />
+                <img src={settingUser?.img} alt="user" />
               </div>
               <h5 className="user-name">{settingUser?.name}</h5>
               <h6 className="user-email">{settingUser?.email}</h6>
             </div>
+            <div className="exit">
+              <h5>Cerrar Session</h5>
+              <Button onClick={handleClick}>
+                <BiLogOut />
+              </Button>
+            </div>
           </div>
-          <Button onClick={handleClick}>
-            <BiLogOut />
-          </Button>
         </div>
-        <div class="card h-100">
-          <h6 className="title">Personal Details</h6>
-          <div className="form-group">
-            <label for="fullName">Full Name</label>
+        <div class="card2">
+          <h6 className="title2">Personal Details</h6>
+          <div className="image">
+            <label for="img">Changed Image</label>
+            <input
+              type="file"
+              className="form-control"
+              id="image"
+              placeholder="Select Picture"
+            />
+          </div>
+          <div className="name">
+            <label for="fullName">Name</label>
             <input
               type="text"
               className="form-control"
               id="fullName"
-              placeholder="Enter full name"
+              placeholder="Enter Name"
             />
           </div>
-          <div className="form-group">
-            <label for="fullName">Email</label>
+          <div className="email">
+            <label for="email">Email</label>
             <input
               type="Email"
               className="form-control"
@@ -62,8 +80,8 @@ function EditUser() {
               placeholder="Enter Email"
             />
           </div>
-          <div className="form-group">
-            <label for="fullName">Password</label>
+          <div className="password">
+            <label for="password">Password</label>
             <input
               type="password"
               className="form-control"
@@ -76,16 +94,12 @@ function EditUser() {
               type="button"
               id="submit"
               name="submit"
-              className="btn btn-secondary"
+              className="cancel"
+              onClick={handleReturn}
             >
               Cancel
             </button>
-            <button
-              type="button"
-              id="submit"
-              name="submit"
-              className="btn btn-primary"
-            >
+            <button type="button" id="submit" name="submit" className="update">
               Update
             </button>
           </div>
@@ -95,11 +109,30 @@ function EditUser() {
   );
 }
 
+const Title = styled.div`
+  .title {
+    background-color: #1a233a;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    color: violet;
+    text-transform: uppercase;
+    padding-top: 17px;
+  }
+`;
+
 const Container = styled.div`
   background-color: #1a233a;
   color: #bcd0f7;
   height: 100vh;
   width: 100vw;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+
   .user-profile {
     margin: 0 0 1rem 0;
     padding-bottom: 1rem;
@@ -109,43 +142,133 @@ const Container = styled.div`
       img {
         width: 90px;
         height: 90px;
-        -webkit-border-radius: 100px;
-        -moz-border-radius: 100px;
         border-radius: 100px;
       }
     }
     .user-name {
       margin: 0 0 0.5rem 0;
+      font-size: 19px;
     }
     .user-email {
       margin: 0;
-      font-size: 0.8rem;
+      font-size: 14px;
       font-weight: 400;
     }
   }
-  .card {
+
+  .exit {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    h5 {
+      font-size: 15px;
+      padding-right: 10px;
+    }
+  }
+
+  .card1 {
     background: #272e48;
     -webkit-border-radius: 5px;
     -moz-border-radius: 5px;
     border-radius: 5px;
     border: 0;
-    margin-bottom: 200px;
+    margin: 0;
     display: flex;
     flex-wrap: wrap;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     align-content: center;
+    padding: 83px;
   }
 
-  .form-control {
-    border: 1px solid #596280;
-    -webkit-border-radius: 2px;
-    -moz-border-radius: 2px;
-    border-radius: 2px;
-    font-size: 0.825rem;
-    background: #1a233a;
-    color: #bcd0f7;
+  .card2 {
+    background: #272e48;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    border: 0;
+    margin: 0;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    padding: 75px;
+    margin-left: 70px;
+    .title2 {
+      font-size: 25px;
+      color: violet;
+    }
+  }
+
+  .image {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .name {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .email {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .password {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .buttons {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    padding-top: 20px;
+    .cancel {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0.5rem;
+      border-radius: 5px;
+      margin-right: 5px;
+      background-color: #9186f3;
+      cursor: pointer;
+      border: none;
+      font-size: 16px;
+      color: #ebe7ff;
+      transition: 0.5s ease-in-out;
+      :hover {
+        background-color: red;
+      }
+    }
+    .update {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0.5rem;
+      border-radius: 5px;
+      margin-right: 5px;
+      background-color: #9186f3;
+      cursor: pointer;
+      border: none;
+      font-size: 16px;
+      color: #ebe7ff;
+      transition: 0.5s ease-in-out;
+      :hover {
+        background-color: green;
+      }
+    }
+  }
+
+  @media screen and (min-width: 639) and (max-width: 1080px) {
+    background: #272e48;
+    height: 100vh;
+    width: 100vw;
   }
 `;
 
@@ -159,6 +282,10 @@ const Button = styled.button`
   background-color: #9186f3;
   cursor: pointer;
   border: none;
+  transition: 0.5s ease-in-out;
+  :hover {
+    background-color: red;
+  }
   svg {
     font-size: 25px;
     color: #ebe7ff;
