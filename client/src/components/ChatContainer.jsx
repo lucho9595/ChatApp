6b0f9af6135fdb714f855859318c6791a86aeafd
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2023-05-05 16:55:57 -0300

    update

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -24,6 +24,18 @@ export default function ChatContainer({ currentChat }) {
 };
 
 const Container = styled.div`
-    
+    padding-top: 1rem;
+    .chat-header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 2rem;
+        .user-details{
+            display: flex;
+            align-items: center;
+            gap: 1rem;
+            .avatar
+        }
+    }
     
 `
